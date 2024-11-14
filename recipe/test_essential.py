7b3efa94d574c67179fa9d577a44982900bc2fa6
Matt Chan <49702524+thewchan@users.noreply.github.com>
2024-11-14 17:35:10 -0500

    Use ESSENTIAL due to mypy_boto3_builder 5.5.0

--- recipe/test_essential.py
+++ recipe/test_essential.py
@@ -3,7 +3,7 @@ from importlib.metadata import requires
 
 from mypy_boto3_builder.service_name import ServiceName
 
-essential_service_names = ServiceName.ESSENTIAL_NAMES
+essential_service_names = ServiceName.ESSENTIAL
 
 for name in essential_service_names:
     res = requires("mypy_boto3_" + name)
